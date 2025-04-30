3bb7ff2b374dcbe65500cc5abf5c29214ca34b08
SolaDevSolide <SolaDevSolide@users.noreply.github.com>
2025-04-30 19:38:21 +0000

    deploy: 0568c6310e7eda75b407c03db0ed307140d58199

---
+++ assets/js/1e3b512d.4264f9cf.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[802],{173:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/CorpoManager/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.1536a556.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/CorpoManager/blog","blogTitle":"Blog","authorsListPath":"/CorpoManager/blog/authors"}')}}]);
